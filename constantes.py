48851a6907cc778be4c609d5478553941e24d8b4
NinonFol <112973194+NinonFol@users.noreply.github.com>
2023-05-03 10:46:26 +0200

    Add files via upload

--- constantes.py
+++ constantes.py
@@ -4,9 +4,9 @@ def membres_groupe():
     return ['Vincent Abdou Chacourou', 'Jys Seyler', 'Ninon Fol', 'Encadrés par Robin Trinh']
 
 # Importation du jeu de données 2020
-def import_data():
+def import_data(link_file):
     import pandas as pd
-    df = pd.read_csv('kaggle_survey_2020_responses.csv')
+    df = pd.read_csv(link_file)
     return df
 
 #Texte à renseigner en introduction du projet
@@ -19,38 +19,9 @@ def distribution(df):
     distribution = df.Q5.value_counts()
     return distribution
 
-#Preprocessing du DF
-def pretraitement(df):
-    import pandas as pd
-    questions=pd.DataFrame(df.loc[0,:]).T
-    df=df.iloc[1:,:]
-    #Convertir la colonne des temps de réponse en nombres
-    df['Time from Start to Finish (seconds)']=df['Time from Start to Finish (seconds)'].astype(float)
 
-    #Filtrage des temps de réponse au questionnaire : garder uniquement les valeurs entre q1 et q3
-    q1 = df['Time from Start to Finish (seconds)'].quantile(0.25)
-    q3 = df['Time from Start to Finish (seconds)'].quantile(0.75)
-    iqr = q3-q1
-    low = q1 - 3*(iqr)
-    high = q3 + 3*(iqr)
-    df = df[df['Time from Start to Finish (seconds)'].between(low, high)]
-
-    # Suppression de la colonne 'Duration'
-    df=df.drop('Time from Start to Finish (seconds)',axis=1)
-
-    #suppression des lignes vides de Q5
-    df=df.dropna(subset=['Q5']) 
-
-    # conserver uniquement lignes avec moins de 90% de nan
-    df=df[df.isna().mean(axis=1)<0.90]
-
-
-
-
-    return df
 
 #MODELISATION
-
 def train_model(model_choisi, X_train, y_train, X_test, y_test) :
     from sklearn.model_selection import train_test_split 
     from sklearn.linear_model import LogisticRegression
@@ -58,12 +29,21 @@ def train_model(model_choisi, X_train, y_train, X_test, y_test) :
     from sklearn.neighbors import KNeighborsClassifier
     from sklearn.ensemble import RandomForestRegressor
     from sklearn import svm
-    if model_choisi == 'Random Forest' : 
+    model = LogisticRegression() #modèle choisi par défaut
+    if model_choisi == 'Logistic Regression' : 
+        model = LogisticRegression()
+    elif model_choisi == 'Random Forest' : 
         model = RandomForestRegressor()
     elif model_choisi == 'Decision Tree' : 
         model = DecisionTreeClassifier()
     elif model_choisi == 'KNN' : 
         model = KNeighborsClassifier()
     model.fit(X_train, y_train)
-    score = model.score(X_test, y_test)
-    return score
+    y_pred = model.predict(X_test)
+    score_train = model.score(X_train, y_train)
+    score_test = model.score(X_test, y_test)
+    dico_result = {'Score Train':score_train,
+                   'Score Test':score_test,
+                   'Predictions':y_pred,
+                   'Model':model}
+    return dico_result
